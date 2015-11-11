28943f3759fbbea5e81de8002b70b48bfa90efda
timwis <tim@timwis.com>
2015-11-11 11:34:18 -0500

    Use local geojson file in demo

--- demo/demo.js
+++ demo/demo.js
@@ -1,4 +1,4 @@
-var map = L.map('map').setView([39.95, -75.1667], 11)
+var map = L.map('map').setView([39.9897471840457, -75.13893127441406], 11)
 
 // Add basemap
 L.tileLayer('http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png', {
@@ -7,26 +7,9 @@ L.tileLayer('http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png', {
 }).addTo(map)
 
 // Add GeoJSON
-$.when(
-	$.getJSON('https://data.phila.gov/resource/bbgf-pidf.geojson'),
-	$.getJSON('https://data.phila.gov/resource/r24g-zx3n.json?%24select=count(*)%20as%20value%2C%20%3A%40computed_region_bbgf_pidf%20as%20label&%24group=%3A%40computed_region_bbgf_pidf&%24order=value%20desc')
-).done(function(responseGeojson, responseData) {
-	var data = responseData[0]
-	var geojson = responseGeojson[0]
-	
-	// Create hash table for easy reference
-	var dataHash = {}
-	data.forEach(function(item) {
-		if(item.label) dataHash[item.label] = item.value
-	})
-	
-	// Add value from hash table to geojson properties
-	geojson.features.forEach(function(item) {
-		item.properties.value = dataHash[item.properties._feature_id] || null 
-	})
-	
+$.getJSON('./crimes_by_district.geojson', function(geojson) {	
 	L.choropleth(geojson, {
-		valueProperty: 'value',
+		valueProperty: 'incidents',
 		scale: ['white', 'red'],
 		steps: 5,
 		mode: 'q',
@@ -36,7 +19,7 @@ $.when(
 			fillOpacity: 0.8
 		},
 		onEachFeature: function(feature, layer) {
-			layer.bindPopup(feature.properties.value)
+			layer.bindPopup('District ' + feature.properties.dist_num + '<br>' + feature.properties.incidents.toLocaleString() + ' incidents')
 		}
 	}).addTo(map)
 })
